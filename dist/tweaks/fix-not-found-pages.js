8d954c4b382e27e32fd208953f7204af917402c0
DeadPackets <shadowblade7536@gmail.com>
2018-09-18 22:57:32 +0400

    Fixed 404 tweak

--- dist/tweaks/fix-not-found-pages.js
+++ dist/tweaks/fix-not-found-pages.js
@@ -3,7 +3,8 @@ function fixNotFoundPages() {
 		console.warn('No support without persistent login.');
 	} else {
 		if ($('h1:contains(Not Found)').length === 1) {
-			window.history.back();
+			//TODO: Improve this.
+			window.location.replace('https://banner.aus.edu/axp3b21h/owa/twbkwbis.P_WWWLogin');
 		}
 	}
 }
